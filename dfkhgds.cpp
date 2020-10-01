9ac99ee2b1d249cf01e6028eb019473c2286f420
pinkman18 <72201509+pinkman18@users.noreply.github.com>
2020-10-01 17:01:14 +0530

    Add files via upload

---
+++ dfkhgds.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	long long int t;
+	cin>>t;
+	while(t--)
+	{
+		 long long int n;
+		 cin>>n;
+		long long int k=sqrt(n);
+		if(k*k>=n)
+		 cout<<k+k-2<<endl;
+		else if((k*(k+1))>=n)
+		   cout<<k+k+1-2<<endl;
+		else
+		  cout<<k+1+k+1-2<<endl;
+		
+		}
+	
+	
+	
+	
+}
